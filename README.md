ea56bf8dd01b2a20929efadc614c51b420eca8a3
GuidtJ <jeromeguidt@hotmail.fr>
2023-03-13 11:50:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<p align="center">
+    <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" />
+    <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" />
+    <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" />
+    <img src="https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white" />
+</p>
+
+# Mystery number
+
+Create a mini-game to guess a mystery number.
+
+![img](./img/mystery-number.png)
+
+## Instructions
+
+- a number is chosen randomly between 1 and 10
+- we then have up to 3 tries to guess the mystery number
+
+## Bonus
+
+Provide 3 levels of difficulty :
+- easy (3 attempts between 1 and 10)
+- medium (2 attempts between 1 and 50)
+- hard (1 attempt between 1 and 100)
+
+## Authors
+
+- [Nicolas Herbez](https://github.com/nicolas-herbez)
---
+++ js/script.js
@@ -0,0 +1,49 @@
+const max = 10;
+let secretNumber = Math.floor(Math.random() * max) + 1;
+let numberInput = document.getElementById("number");
+let generateBtn = document.getElementById("generateBtn");
+let resetBtn = document.getElementById("reset");
+let essais = [
+  document.getElementById("essai1"),
+  document.getElementById("essai2"),
+  document.getElementById("essai3"),
+];
+let essaisCounter = 0;
+
+generateBtn.addEventListener("click", function() {
+  let choixUtilisateur = parseInt(numberInput.value);
+  
+  if (essaisCounter < 3) {
+    if (choixUtilisateur === secretNumber) {
+      essais[essaisCounter].textContent = "Gagné 🙂 ! le numéro mystère était " + secretNumber + " ";
+      generateBtn.disabled = true;
+      resetBtn.style.display = "block";
+    } else {
+      if (choixUtilisateur < secretNumber) {
+        essais[essaisCounter].textContent = "C'est ➕ que : " + choixUtilisateur + " ";
+      } else {
+        essais[essaisCounter].textContent = "C'est ➖ que : " + choixUtilisateur + " ";
+      }
+      essaisCounter++;
+    }
+  }
+  
+  if (essaisCounter === 3) {
+    essais[essaisCounter-1].textContent = "YOU LOSE 😔 ! Le nombre mystère était " + secretNumber + ".";
+    generateBtn.disabled = true;
+    resetBtn.style.display = "block";
+  }
+  
+  numberInput.value = "";
+});
+
+resetBtn.addEventListener("click", function() {
+  generateBtn.disabled = false;
+  essaisCounter = 0;
+  secretNumber = Math.floor(Math.random() * max) + 1;
+  for (let i = 0; i < 3; i++) {
+    essais[i].innerHTML = "";
+  }
+  numberInput.value = "";
+  resetBtn.style.display = "none";
+});
